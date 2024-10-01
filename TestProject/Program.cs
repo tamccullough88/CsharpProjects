cd961eec05873099f4026297e7097fd35e88e85a
Thomas McCullough <tamccullough88@gmail.com>
2024-09-30 21:32:06 -0400

    Exercise - Complete a challenge activity using Boolean expressions. Select greeting based on admin|manager level

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,3 +1,30 @@
-﻿Random coin = new Random();
-int side = coin.Next(0, 2);
-Console.WriteLine((side == 0) ? "heads" : "tails");
+﻿string permission = "Admin|Manager";
+int level = 55;
+
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
