9c50495d64bd8ae5fb01f9152b55ed62066e9c22
Thomas McCullough <tamccullough88@gmail.com>
2024-09-30 21:03:23 -0400

    Conditional Operators

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,17 +1,3 @@
-﻿// Console.WriteLine(1 > 2);
-// Console.WriteLine(1 < 2);
-// Console.WriteLine(1 >= 1);
-// Console.WriteLine(1 <= 1);
-
-// string pangram = "The quick brown fox jumps over the lazy dog.";
-// Console.WriteLine(pangram.Contains("fox"));
-// Console.WriteLine(!pangram.Contains("cow") == false);
-// Console.WriteLine(pangram.Contains("fox") != false);
-// Console.WriteLine(!pangram.Contains("fox"));
-
-int a = 7;
-int b = 6;
-Console.WriteLine(a != b); // output: True
-string s1 = "Hello";
-string s2 = "Hello";
-Console.WriteLine(s1 != s2); // output: False
+﻿int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
