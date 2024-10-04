064a3d10fb2bf0b9ba9d7ea2104368f64ffb94cf
Thomas McCullough <tamccullough88@gmail.com>
2024-10-03 21:28:46 -0400

    app closed and on reopen had debug update

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d1f68baf1305acb9103d5de5de320c0cd494b5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd961eec05873099f4026297e7097fd35e88e85a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
