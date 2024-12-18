99e405beb1e85ad9622b9f1eecd472a6df1da3c8
Thomas McCullough <tamccullough88@gmail.com>
2024-12-17 20:39:16 -0500

    vscode update

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f63262cac43464519b35237859fb48c91f645eae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c98c8dc59716a5fe41dcb4be4b41a613ba629c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
