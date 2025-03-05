126701c390a63fa06c85842345ac902d4cc9eb17
tamccullough88 <tamccullough88@gmail.com>
2025-03-05 16:53:15 -0500

    converting and dividing numbers

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,22 +1,19 @@
 ﻿using System.Runtime.CompilerServices;
 
-string[] values = { "abc", "12.3", "45", "11", "def"};
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-decimal total = 0m;
-string message = "";
+// Your code here to set result1
 
-foreach (var value in values)
-{
-    decimal number;
-    if (decimal.TryParse(value, out number)) 
-    {   
-        total += number;
-    } else 
-    {
-        message += value;
+int result1 = Convert.ToInt32(value1 / value2);
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-    }
-}
+// Your code here to set result2
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-Console.WriteLine($"message: {message}");
-Console.WriteLine($"Total: {total}");
+//Your code here to set result3
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d372b1c9817ce9489c5321c5f42168a99c5465e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aac6a0599dd4b52615c25157cfbe251c54da9f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
